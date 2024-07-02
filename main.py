fe4df7d17b8e099c5f1d659d10c600f2d6dce340
linet4 <linetwahome2017@gmail.com>
2024-07-02 21:31:15 +0300

    update

--- main.py
+++ main.py
@@ -27,73 +27,76 @@ def products():
 
 @app.route("/sales")
 def sales():
-    products=get_data("products")
-    sals=get_data("sales")
-    return render_template("sales.html",sals=sals,products=products)
+    if 'email' in session:
+      products=get_data("products")
+      sals=get_data("sales")
+      return render_template("sales.html",sals=sals,products=products)
+    else:
+        flash('login to view page')
+        return redirect(url_for('login'))
 
 @app.route("/dashboard")
 def dashboard():
-    s_product=sales_product()
-    # print(s_product)
-    p_name=[]
-    s_p=[]
-    for i in s_product:
-        p_name.append(i[1])
-        s_p.append(i[0])
-    s_profit=profit() 
-    for i in s_profit:
-        pr=[]
-        pr.append(float(i[1]))
+    if 'email' in session:
+        s_product=sales_product()
+        # print(s_product)
+        p_name=[]
+        s_p=[]
+        for i in s_product:
+            p_name.append(i[1])
+            s_p.append(i[0])
+        s_profit=profit() 
+        for i in s_profit:
+            pr=[]
+            pr.append(float(i[1]))
 
-    # Sales per Day
-    s_day=sales_day()
-    # print(s_day)
-    date=[]
-    s_d=[]
-    for i in s_day:
-        date.append(str(i[0]))
-        s_d.append(float(i[1]))
-    
-    # profit per Day
-    p_day=profit_daily()
-    # print(p_day)
-    p_dy=[]
-    for i in p_day:
-      p_dy.append(float(i[1]))  
-
-    # total sales
-    t_sales=total_sales()
-    # print(t_sales)
-    for i in t_sales:
-        sales_t=round(i[0],2)
-         
-    # print(sales_t)
-    # today sales
-    s_daily=today_sales()
-    # print(s_daily)
-    for i in s_daily:
-        Todaysales=(float(i[1]))
-    # Total profit
-    t_profit=total_profit()
-    # print(t_profit)
-    for i in t_profit:
-        Totalprofit=i[0]
-    #Today profit
-    today_p=today_profit()
-    for i in today_p:
-        todayprofit=round(i[1],2)
-    # print(todayprofit) 
-    # Resent sales
-    r_sales=recent_sales()
-    # print(r_sales)
-    
-
-     
-       
+        # Sales per Day
+        s_day=sales_day()
+        # print(s_day)
+        date=[]
+        s_d=[]
+        for i in s_day:
+            date.append(str(i[0]))
+            s_d.append(float(i[1]))
         
-    
-    return render_template("dashboard.html",p_name=p_name,s_p=s_p,pr=pr,
-                           s_d=s_d,date=date,p_dy=p_dy,sales_t=sales_t,Todaysales=Todaysales,Totalprofit=Totalprofit,todayprofit=todayprofit,r_sales=r_sales)
+        # profit per Day
+        p_day=profit_daily()
+        # print(p_day)
+        p_dy=[]
+        for i in p_day:
+         p_dy.append(float(i[1]))  
+
+        # total sales
+        t_sales=total_sales()
+        # print(t_sales)
+        for i in t_sales:
+            sales_t=round(i[0],2)
+            
+        # print(sales_t)
+        # today sales
+        s_daily=today_sales()
+        # print(s_daily)
+        for i in s_daily:
+            Todaysales=(float(i[1]))
+        # Total profit
+        t_profit=total_profit()
+        # print(t_profit)
+        for i in t_profit:
+            Totalprofit=i[0]
+        #Today profit
+        today_p=today_profit()
+        for i in today_p:
+            todayprofit=round(i[1],2)
+        # print(todayprofit) 
+        # Resent sales
+        r_sales=recent_sales()
+        # print(r_sales)
+
+        return render_template("dashboard.html",p_name=p_name,s_p=s_p,pr=pr,
+                            s_d=s_d,date=date,p_dy=p_dy,sales_t=sales_t,Todaysales=Todaysales,Totalprofit=Totalprofit,todayprofit=todayprofit,r_sales=r_sales)
+    else:
+        flash('login to view page')
+        return redirect(url_for('login'))
                            
 @app.route("/login",methods=["POST","GET"])
 def login():
